e5739b9b4ff16e6b2cf4fa9b32cdf6cf067cfccc
PassiHD <59457929+PassiHD2004@users.noreply.github.com>
2022-08-11 23:20:27 +0200

    Delete target directory

--- target/debug/deps/fast_webserver-46f5aa99f6412124.d
+++
@@ -1,5 +0,0 @@
-/home/passihd/Coding/rust-projects/fast-webserver/target/debug/deps/fast_webserver-46f5aa99f6412124: src/main.rs
-
-/home/passihd/Coding/rust-projects/fast-webserver/target/debug/deps/fast_webserver-46f5aa99f6412124.d: src/main.rs
-
-src/main.rs:
--- target/debug/fast-webserver.d
+++
@@ -1,1 +0,0 @@
-/home/passihd/Coding/rust-projects/fast-webserver/target/debug/fast-webserver: /home/passihd/Coding/rust-projects/fast-webserver/src/main.rs
